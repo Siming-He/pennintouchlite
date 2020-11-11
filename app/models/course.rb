3bc8dc909145215aef5a170d67f446c6350dbc45
SimonHe <siminghe2020@gmail.com>
2020-11-11 12:36:01 +0800

    Model creation and course methods

---
+++ app/models/course.rb
@@ -0,0 +1,28 @@
+class Course < ApplicationRecord
+  belongs_to :department
+  has_many :registrations, dependent: :destroy
+  has_many :users, through: :registrations
+  validates :department, :code, :title, :description, presence: true
+  validates :department_id, uniqueness: {scope: :code}
+
+  def full_code
+    Department.where(id: department_id)[0][:code] + '-' + code.to_s
+  end 
+
+  def instructor
+    course = Course.find(id)
+    course.users.find_by(is_instructor: true)
+  end
+
+  def instructor=(user)
+    if user.is_instructor
+      Registration.create({course: Course.find(id), user: user})
+    end
+  end
+
+  def students
+    course = Course.find(id)
+    course.users.where(is_instructor: false)
+  end
+
+end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,3 @@
 class Department < ApplicationRecord
-  # TODO: add one-to-many association
+  has_many :courses
 end
